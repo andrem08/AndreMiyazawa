553cb61e9e523e6784df0f3f46b89eaeca3426e9
André Miyazawa <andre08.m@usp.br>
2024-03-09 21:58:35 -0300

    Inserindo os primeiros arquivos do site

--- scripts.js
+++ scripts.js
@@ -17,4 +17,14 @@ window.onload = function() {
         .then(data => {
             document.getElementById('head').innerHTML = data;
         });
+    fetch('header/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+    fetch('head/head.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('head').innerHTML = data;
+        });
 };
