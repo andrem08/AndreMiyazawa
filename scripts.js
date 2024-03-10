27c8c31a8979c10615f19dc1e14a017fa45367fd
André Miyazawa <andre08.m@usp.br>
2024-03-10 05:20:45 -0300

    Inserindo os primeiros arquivos do site

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-    loadHTML('/Meu-Site-Pessoal/header.html', 'header');
-    loadHTML('/Meu-Site-Pessoal/head.html', 'head');
-    loadHTML('/Meu-Site-Pessoal/footer.html', 'footer');
+    loadHTML('header.html', 'header');
+    loadHTML('head.html', 'head');
+    loadHTML('footer.html', 'footer');
+
 };
 
 // Utilizar o fetch para enviar o header em todas as páginas
@@ -10,11 +11,20 @@ window.onload = function() {
 // O response.text() retorna o conteúdo do arquivo como texto
 // O data é o conteúdo do arquivo
 // O document.getElementById('header').innerHTML = data; insere o conteúdo do arquivo no elemento com id 'header'
-
 function loadHTML(path, elementId) {
-    fetch(path)
+    fetch('/Meu-Site-Pessoal/' + path)
+        .then(response => {
+            if (!response.ok) {
+                // If the file does not exist, modify the path
+                return fetch(path);
+            }
+            return response.text();
+        })
         .then(response => response.text())
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.log(`An error occurred: ${error.message}`);
         });
 }
